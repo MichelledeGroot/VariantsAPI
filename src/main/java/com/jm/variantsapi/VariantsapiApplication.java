f71d82a74e63a732af98a243d169097955f11c60
michelle <M.deGroot5@student.han.nl>
2020-04-23 14:01:00 +0200

    spring framework start

---
+++ src/main/java/com/jm/variantsapi/VariantsapiApplication.java
@@ -0,0 +1,13 @@
+package com.jm.variantsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VariantsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VariantsapiApplication.class, args);
+	}
+
+}
